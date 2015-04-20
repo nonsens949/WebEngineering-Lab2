970e76ae2900940374566e6be797ccf4fbb6a177
Emil Gruber <e1049009@student.tuwien.ac.at>
2015-04-20 23:32:22 +0200

    added implements and extends statements to BigJeopardyServlet, created method heads for init and doGet

--- lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/BigJeopardyServlet.java
+++ lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/BigJeopardyServlet.java
@@ -1,5 +1,23 @@
 package at.ac.tuwien.big.we15.lab2.servlet;
 
-public class BigJeopardyServlet {
+import java.io.IOException;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BigJeopardyServlet extends HttpServlet implements Servlet {
+
+	@Override
+	public void init() throws ServletException {
+		//Initializations
+		String p = getInitParameter("initpara1");
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//called by server (via service method) to allow the servlet to handle a GET request
+	}
 }
