bd233ebb02fafb8049776adb7204c1bd5de2af15
Birgit Ch <birgit.chmelar@tuwien.ac.at>
2015-04-21 13:13:55 +0200

    web.xml geändert um BigJeopardyServlet einzubinden Startseite ist nun login.jsp

--- lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/BigJeopardyServlet.java
+++ lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/BigJeopardyServlet.java
@@ -13,11 +13,20 @@ public class BigJeopardyServlet extends HttpServlet implements Servlet {
 	@Override
 	public void init() throws ServletException {
 		//Initializations
+		System.out.println("init servlet");
 		String p = getInitParameter("initpara1");
 	}
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//called by server (via service method) to allow the servlet to handle a GET request
+		System.out.println("doGet in servlet");
 	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//TODO
+		System.out.println("doPost in servlet");
+	}
+	
 }
